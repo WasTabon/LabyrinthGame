abfe703de324f2e7144308442449355143c2cd83
WasTabon <titanium.jen@gmail.com>
2025-08-28 19:37:03 +0300

    scene

--- Assets/LabyrinthGame/Scripts/LevelController.cs
+++ Assets/LabyrinthGame/Scripts/LevelController.cs
@@ -2,40 +2,39 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-   public static LevelController Instance;
+    public static LevelController Instance;
 
-   public GameObject _maze0f;
-   public GameObject _maze90f;
-   public GameObject _maze180f;
-   public GameObject _mazeM90f;
+    public GameObject _maze0f;
+    public GameObject _maze90f;
+    public GameObject _maze180f;
+    public GameObject _mazeM90f;
 
-   private void Awake()
-   {
-      Instance = this;
-   }
-   
-   public void ActivateMaze(MazeType maze)
-   {
-      // Выключаем все
-      _maze0f.SetActive(false);
-      _maze90f.SetActive(false);
-      _maze180f.SetActive(false);
-      _mazeM90f.SetActive(false);
-      
-      switch (maze)
-      {
-         case MazeType.m0f:
-            _maze0f.SetActive(true);
-            break;
-         case MazeType.m90f:
-            _maze90f.SetActive(true);
-            break;
-         case MazeType.m180f:
-            _maze180f.SetActive(true);
-            break;
-         case MazeType.mN90f:
-            _mazeM90f.SetActive(true);
-            break;
-      }
-   }
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ActivateMaze(MazeType maze)
+    {
+        if (_maze0f != null) _maze0f.SetActive(false);
+        if (_maze90f != null) _maze90f.SetActive(false);
+        if (_maze180f != null) _maze180f.SetActive(false);
+        if (_mazeM90f != null) _mazeM90f.SetActive(false);
+        
+        switch (maze)
+        {
+            case MazeType.m0f:
+                if (_maze0f != null) _maze0f.SetActive(true);
+                break;
+            case MazeType.m90f:
+                if (_maze90f != null) _maze90f.SetActive(true);
+                break;
+            case MazeType.m180f:
+                if (_maze180f != null) _maze180f.SetActive(true);
+                break;
+            case MazeType.mN90f:
+                if (_mazeM90f != null) _mazeM90f.SetActive(true);
+                break;
+        }
+    }
 }
